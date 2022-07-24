c4b5b2c6351378d496a2009ad84d1d390cd534b0
Abubaker Siddique <Abubakersiddique761@gmail.com>
2022-07-24 14:24:33 +0530

    Finished

---
+++ src/JS/main.js
@@ -0,0 +1,131 @@
+let QRCode = require('qrcode')
+let { ipcRenderer, shell, BrowserWindow, screen } = require('electron');
+
+let canvas = document.getElementById('canvas')
+let input = document.getElementById("input")
+let generate_btn = document.getElementById("generate")
+
+// For Button Disable
+setInterval(() => {
+    if (input.value.length == 0) {
+        generate_btn.disabled = true
+    } else {
+        generate_btn.disabled = false
+    }
+}, 250);
+
+
+// Values
+let color;
+let bg;
+let scale;
+let margin;
+let mp;
+let r = document.querySelector(':root');
+let main_url;
+
+setInterval(() => {
+    color = document.getElementById("color").value
+    bg = document.getElementById("bg").value
+    scale = document.getElementById("scale").value
+    margin = document.getElementById("margin").value
+    mp = document.getElementById("mp").value
+
+
+    // For Dark Mode
+    let DarkMode = document.getElementById("DarkMode").checked;
+
+    if (DarkMode == true) {
+        r.style.setProperty('--theme-background', '#191919');
+        r.style.setProperty('--theme-background-2', '#121212');
+        r.style.setProperty('--editor-background', '#202020');
+        r.style.setProperty('--theme-color', 'white');
+        r.style.setProperty('--min-color', 'lightblue');
+        r.style.setProperty('--max-color', 'lightgreen');
+        r.style.setProperty('--shadow-color', '#191919');
+    } else {
+        r.style.setProperty('--theme-background', 'lightgray');
+        r.style.setProperty('--theme-background-2', 'white');
+        r.style.setProperty('--editor-background', '#F0F0F0');
+        r.style.setProperty('--theme-color', 'black');
+        r.style.setProperty('--min-color', 'blue');
+        r.style.setProperty('--max-color', 'green');
+        r.style.setProperty('--shadow-color', 'darkgray');
+    }
+
+
+}, 250);
+
+
+// Event on Generate Button is Clicked
+generate_btn.addEventListener("click", () => {
+    console.log(color, bg, scale, margin, mp)
+    QRCode.toCanvas(canvas, input.value, {
+        color: {
+            dark: color,
+            light: bg,
+        },
+        scale: scale,
+        margin: margin,
+        maskPattern: mp,
+    },
+        function (error) {
+            if (error) console.error(error)
+        })
+
+    QRCode.toDataURL(input.value)
+        .then(url => {
+            console.log(url)
+            main_url = url
+        })
+        .catch(err => {
+            console.error(err)
+        })
+})
+
+
+// Title Bar JS
+document.getElementById("quit-button").addEventListener("click", () => {
+    ipcRenderer.send('quit-app')
+})
+
+document.getElementById("refresh_btn").addEventListener("click", () => {
+    ipcRenderer.send('reload-app')
+})
+
+document.getElementById("minimize-button").addEventListener("click", () => {
+    ipcRenderer.send('minimize-app')
+})
+
+// document.getElementById("maximize-button").addEventListener("click", () => {
+//     ipcRenderer.send('maximize-app')
+//     ipcRenderer.on("maximizing-icon", (event, icon) => {
+//         // if (icon == "Max") {
+//         //     document.getElementById("maximize-button").innerHTML = `<i class="bi bi-app"></i>`
+//         // } else if (icon == "UnMax") {
+//         //     document.getElementById("maximize-button").innerHTML = `<i class="bi bi-app-indicator"></i>`
+//         // }
+//     })
+// })
+
+// ipcRenderer.on('asynchronous-message', function (evt, message) {
+//     let icon = message.icon;
+//     // console.log(icon)
+//     // if (icon == "Max") {
+//     //     document.getElementById("maximize-button").innerHTML = `<i class="bi bi-app-indicator"></i>`
+//     // } else if (icon == "UnMax") {
+//     //     document.getElementById("maximize-button").innerHTML = `<i class="bi bi-app"></i>`
+//     // }
+// });
+
+
+// Function to Downlaod an Image 
+
+let downloadImage = () => {
+    // let url = "https://www.timeoutdubai.com/cloud/timeoutdubai/2021/09/11/hfpqyV7B-IMG-Dubai-UAE.jpg"
+    ipcRenderer.send('download-img', {
+        payload: {
+            url: main_url
+        }
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, globalShortcut, dialog } = require('electron');
 const path = require('path');
+let { download } = require('electron-dl')
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -11,20 +12,105 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 530,
+    minWidth: 800,
+    minHeight: 530,
+    maxWidth: 800,
+    maxHeight: 530,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+    frame: false,
+    maximizable: false,
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools()
+
 };
+
+ipcMain.on('quit-app', (event) => {
+  app.quit()
+})
+
+ipcMain.on('minimize-app', (event) => {
+  BrowserWindow.getFocusedWindow().minimize()
+})
+
+ipcMain.on('reload-app', (event) => {
+  BrowserWindow.getFocusedWindow().reload()
+})
+
+ipcMain.on('maximize-app', (event) => {
+  let icon = ""
+  if (BrowserWindow.getFocusedWindow().isMaximized()) {
+    BrowserWindow.getFocusedWindow().unmaximize()
+    icon = "Max"
+  } else {
+    BrowserWindow.getFocusedWindow().maximize()
+    icon = "UnMax"
+  }
+
+})
+
+// IPC Main to Download an Image File
+ipcMain.on('download-img', (event, { payload }) => {
+
+  dialog.showSaveDialog({
+    title: 'Save Image - GenerateX',
+    buttonLabel: 'Save',
+    // Restricting the user to only Text Files.
+    filters: [
+      {
+        name: 'PNG File',
+        extensions: ['png']
+      },],
+    properties: []
+  }).then(file => {
+    // Stating whether dialog operation was cancelled or not.
+    console.log(file.canceled);
+    if (!file.canceled) {
+      console.log(file.filePath.toString());
+
+      // Creating and Writing to the sample.html file
+      // fs.writeFile(file.filePath.toString(), text, function (err) {
+      //   if (err) throw err;
+      //   console.log('Saved!');
+      // });
+      console.log(BrowserWindow.getFocusedWindow())
+      console.log(payload.url)
+      try {
+        download(BrowserWindow.getFocusedWindow(), payload.url, { directory: file.filePath.toString() }).then(
+          console.log("Downloaded")
+        )
+      } catch (error) {
+        console.log("Error: \n")
+        console.log(error)
+      }
+
+    }
+  }).catch(err => {
+    console.log(err)
+  });
+
+
+})
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
+
+app.on('ready', () => {
+  globalShortcut.register('Ctrl+Shift+I', () => {
+    console.log('Ctrl+Shift+I is pressed')
+  })
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
